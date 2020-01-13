3949cba333f57180d2dca6c779eb07f2a952d754
Andrew Chan <achan@nisum.com>
2020-01-13 11:16:49 -0800

    Finish jsx explanation

---
+++ jsx.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const JSXDemo = () => { // With React Hooks
+    const [username, setUsername] = useState[''];
+
+    return (
+        <input 
+            id="username" 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.currentTarget.value)} // Set state in line with arrow function
+        />
+    )
+}
+
+const vanillaDemo = () => { // Without React, DOM manipulation
+    let username = ""; // Simulate initial state
+
+    const setUsername = (updatedUsername) => {
+        username = updatedUsername; // Simulate setState
+        usernameInput.value = updatedUsername; // Simulate rerender, force refresh
+    }
+
+    const usernameInput = document.getElementById("username"); // Grab the relevant component
+
+    usernameInput.onchange = (e) => setUsername(usernameInput.value); // Attach onChange handler to component
+}
